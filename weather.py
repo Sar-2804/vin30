96b6d3f315208c3c29eb6498f7dd6435d627a100
Sar-2804 <ghadisarvesh477@gmail.com>
2026-07-05 20:25:11 +0530

    Update API key in weather.py

--- weather.py
+++ weather.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 
 # Replace with your OpenWeatherMap API key
-API_KEY = "YOUR_API_KEY"
+API_KEY = "API_KEY = "a1b2c3d4e5f6g7h8i9j0""
 
 st.set_page_config(page_title="Live Weather", page_icon="🌤️")
 
